5f87351f02c688f080860befe393244ae8d0a10f
Chris Clark <chris@untrod.com>
2024-07-03 16:14:41 -0400

    logging to debug the base issue

--- explorer/tests/settings_base.py
+++ explorer/tests/settings_base.py
@@ -2,3 +2,21 @@ from explorer.tests.settings import *  # noqa
 
 EXPLORER_TASKS_ENABLED = False
 EXPLORER_USER_UPLOADS_ENABLED = False
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    },
+}
